a47d6d2859bc79c9c3a4947ff8b9559ffbae9757
atharva-k28 <atharva.kshirsagar2810@gmail.com>
2025-06-17 16:47:04 +0530

    Initial IMG URL to CAPTION testing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { use, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+type formdata = {
+  img_url: string,
+};
 
-  return (
+const NewVision = () =>{
+  const [formData, SetFormData] = useState<formdata>({ img_url: "" });
+  const [outputText,setOutputText] = useState("");
+
+
+  const request= async() =>{
+    const response = await fetch("http://127.0.0.1:8000/process-url",{
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+    const res = await response.json();
+    setOutputText(res.caption);
+  }
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h2>INPUT TRIAL TEXT:</h2>
+    <input type="text" placeholder="enter text" value={formData.img_url} onChange={(e)=>{SetFormData({...formData,img_url:e.target.value});}}/>
+    <button onClick={request}>SUBMIT</button>
+
+    {formData.img_url && (
+  <img src={formData.img_url} alt="Input Preview" width="300" />
+)}
+
+
+    {outputText && <div>
+        <h2>OUTPUT TEXT:</h2>
+        <p>{outputText}</p>
+      </div>}
     </>
   )
 }
 
-export default App
+export default NewVision;
